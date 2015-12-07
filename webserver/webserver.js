eeb4545f9ce7d30e3f7b471fa3ed876ffc19ef01
abhisheksharma18 <abhisheksharma18@gmail.com>
2015-12-07 14:35:34 +0530

    chat server update

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -12,6 +12,27 @@ switch(path){
         res.end();
     break;
 
+    case '/':
+        fs.readFile(__dirname+'/index.html',function(err,data){
+
+            if(err)
+                return send404(res);
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data,'utf8');
+            res.end();
+        });
+        break;
+    case '/twitter':
+            fs.readFile(__dirname+'/twitter.html',function(err,data){
+
+                if(err)
+                    return send404(res);
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.write(data,'utf8');
+                res.end();
+            });
+            break;
+
     default:
     send404(res);
     }
